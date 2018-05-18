81e73bafe8f1a62803b47a3be2cac16bea2d1998
Denis <FunnyDen4uk@gmail.com>
2018-05-18 21:53:42 +0300

    Add files via upload

---
+++ DBMain.java
@@ -0,0 +1,20 @@
+package ru.virtus.pro;
+
+public class DBMain {
+
+	public static void main(String[] args) {
+		DBWorker db = new DBWorker();
+		User user = db.getUserById(4);
+		//System.out.println(user);
+		System.out.println(db.getAccess("Admin", "1234"));
+		User someone = new User();
+		someone.setName("Jack");
+		someone.setLogin("Apple");
+		someone.setPassword("MacBook");
+		db.insertDB(someone);
+		System.out.println(user);
+		
+
+	}
+
+}
